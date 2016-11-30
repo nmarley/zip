a84a666c43da25e24b22c05a67154a43549558ec
Nathan Marley <nathan.marley@gmail.com>
2016-11-29 22:01:37 -0800

    load script

--- load.rb
+++ load.rb
@@ -1,3 +1,41 @@
+require 'csv'
+require 'active_record'
 
+fn = 'zipcode.csv'
 
+# active record magic...
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  host: 'localhost',
+  database: 'test',
+  username: 'test',
+)
+
+class State < ActiveRecord::Base
+  has_many :cities
+end
+class City < ActiveRecord::Base
+  belongs_to :state
+  has_many :zip_codes
+
+  def to_s
+    "#{name}, #{state.abbr}"
+  end
+end
+class ZipCode < ActiveRecord::Base
+  belongs_to :city
+end
+
+CSV.parse(File.read(fn), headers: true).each do |rec|
+  zip = rec['zip']
+
+  state = State.find_or_create_by(abbr: rec['state'])
+  city = City.find_or_create_by(state: state, name: rec['city'])
+  zip_code = ZipCode.find_or_create_by(city: city, code: rec['zip'])
+
+  #puts "city = [#{city}]"
+  #puts "zip = [#{zip}]"
+  #puts "=" * 72
+end
 
